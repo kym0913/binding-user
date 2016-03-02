1c56b8c25a56187aeeb43a32e93374cb568b63d3
kym0913 <km13327911384@gmail.com>
2016-03-02 08:15:36 +0800

    binding user upload

---
+++ application/logs/log-2016-03-02.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-03-02 01:04:45 --> 404 Page Not Found: Faviconico/index
+ERROR - 2016-03-02 01:05:08 --> 404 Page Not Found: Faviconico/index
